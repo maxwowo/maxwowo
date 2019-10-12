b61c670b250c407d8301d6b114a60e28f2fd3dc8
Max <maxwo@protonmail.com>
2019-10-12 23:18:08 +1100

    Made particles repulse upon hover

--- src/Components/Particles/Particles.js
+++ src/Components/Particles/Particles.js
@@ -3,10 +3,27 @@ import ParticlesJs from 'react-particles-js';
 
 const Particles = () => (
   <ParticlesJs
+    params={
+      {
+        particles: {
+          number: {
+            value: 120
+          }
+        },
+        interactivity: {
+          events: {
+            onhover: {
+              enable: true,
+              mode: 'repulse'
+            }
+          }
+        }
+      }
+    }
     style={
       {
-        position: "absolute",
-        zIndex: -100
+        position: 'absolute',
+        // zIndex: -100
       }
     }
   />
