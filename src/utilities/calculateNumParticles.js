2cf46597463a8a1387cbafb591e630a9e0029754
Max <maxwo@protonmail.com>
2019-10-13 21:20:38 +1100

    Aight

--- src/utilities/calculateNumParticles.js
+++ src/utilities/calculateNumParticles.js
@@ -1,6 +1,6 @@
 const calculateNumParticles = () => {
   const numPixels = window.innerHeight * window.innerWidth;
-  return Math.floor(numPixels / 8000);
+  return Math.floor(numPixels / 10000);
 };
 
 export default calculateNumParticles;
