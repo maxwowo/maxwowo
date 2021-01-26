5e794b94a08776467faae4ec9b3647d07a83d09b
maxwowo <maxwo@protonmail.com>
2021-01-26 16:06:06 +1100

    Install TailwindCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import {
   AppComponent,
   AppProps,
 } from 'next/dist/next-server/lib/router/router';
+import "tailwindcss/tailwind.css";
 
 const App: AppComponent = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
