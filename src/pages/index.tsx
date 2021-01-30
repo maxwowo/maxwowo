3a656093d332516c292317300dd58a34cf06b758
maxwowo <maxwo@protonmail.com>
2021-01-30 18:13:27 +1100

    Fix next-seo style bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,6 @@ import Particles from '../components/Particles'
 const Landing: NextPage = () => (
   <Fragment>
     <NextSeo
-      canonical="https://maxwowo.com"
       description="4th year Software Engineering student at UNSW. My specialties include googling error messages and modifying code copied from Stack Overflow. Little known fact: also dope on the mic."
       title="¯\_(ツ)_/¯"
     />
