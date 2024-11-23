30b4d2559d7f6bf81616f9c29f62b8c531278433
Max Wo <hello@maxwowo.com>
2024-11-23 11:56:09 +0800

    refactor: change classname orderings

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,7 +6,7 @@ import { Links } from '@/components/links'
 export const Hero: FunctionComponent = memo(() => (
   <Centered>
     <div className="z-10">
-      <p className="text-7xl leading-none tracking-tight font-medium text-white subpixel-antialiased md:text-10xl">
+      <p className="text-7xl font-medium leading-none tracking-tight text-white subpixel-antialiased md:text-10xl">
         Max Wo
       </p>
       <p className="text-base text-white subpixel-antialiased md:text-3xl">
